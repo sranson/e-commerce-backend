bfa47f2587b9ed947b46d123bc21befe706f2c63
Salome Ranson <salomeranson@gmail.com>
2021-05-13 13:57:47 -0400

    successfully seeding the database with npm run seed command

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: "tag_product",
+  // as: "tag_product",
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: "tag_product",
+  // as: "tag_product",
 });
 
 module.exports = {
