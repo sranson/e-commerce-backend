c2e5327f8597f97b81abd9fe27e60d3fea9ba67c
Salome Ranson <salomeranson@gmail.com>
2021-05-14 06:39:42 -0700

    successfully making api call to grab product based on id through route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,17 +8,38 @@ router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
-    res.status(200).json(productData);
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+        },
+      ],
+    });
+    res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
